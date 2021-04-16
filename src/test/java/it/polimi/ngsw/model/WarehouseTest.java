f39e2c0e9bb8e7c70399c4e2cdd3e0039a645bba
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-04-16 12:13:07 +0200

    WarehouseTest fix

--- src/test/java/it/polimi/ngsw/model/WarehouseTest.java
+++ src/test/java/it/polimi/ngsw/model/WarehouseTest.java
@@ -8,15 +8,6 @@ public class WarehouseTest {
 
     Warehouse warehouse = new Warehouse();
 
-    /**
-    @Test
-    public void activateAbilityTest(){
-        list.add(0, devCard);
-        LeaderCardBaseDecorator leader1 = new DiscountAbility(new ConcreteLeaderCard(), 2, list, Resource.SHIELD);
-        leader1.activateAbility(player, Resource.EMPTY, 0);
-        assert(player.getDiscountPowerOne().equals(Resource.SHIELD));
-    }
-    */
 
     @Test
     public void addResourceToWarehouseTest(){
@@ -29,34 +20,53 @@ public class WarehouseTest {
         assertEquals(warehouse.getShelf(1).getResourceType(), Resource.MONEY);
 
         //test legalità secondo piano
-        warehouse.addResourceToWarehouse(2, Resource.MONEY);
+        warehouse.addResourceToWarehouse(2, Resource.SHIELD);
         assertEquals(warehouse.getShelf(2).getResourceNumber(), 1);
-        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.MONEY);
-        warehouse.addResourceToWarehouse(2, Resource.MONEY);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.SHIELD);
+        warehouse.addResourceToWarehouse(2, Resource.SHIELD);
         assertEquals(warehouse.getShelf(2).getResourceNumber(), 2);
-        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.MONEY);
-        assertEquals(warehouse.addResourceToWarehouse(2,Resource.MONEY), false);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.SHIELD);
+        assertEquals(warehouse.addResourceToWarehouse(2,Resource.SHIELD), false);
         assertEquals(warehouse.getShelf(2).getResourceNumber(), 2);
-        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.MONEY);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.SHIELD);
 
         //test legalità terzo piano
-        warehouse.addResourceToWarehouse(3, Resource.MONEY);
+        warehouse.addResourceToWarehouse(3, Resource.STONE);
         assertEquals(warehouse.getShelf(3).getResourceNumber(), 1);
-        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.MONEY);
-        warehouse.addResourceToWarehouse(3, Resource.MONEY);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        warehouse.addResourceToWarehouse(3, Resource.STONE);
         assertEquals(warehouse.getShelf(3).getResourceNumber(), 2);
-        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.MONEY);
-        warehouse.addResourceToWarehouse(3, Resource.MONEY);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        warehouse.addResourceToWarehouse(3, Resource.STONE);
         assertEquals(warehouse.getShelf(3).getResourceNumber(), 3);
-        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.MONEY);
-        assertEquals(warehouse.addResourceToWarehouse(3,Resource.MONEY), false);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        assertEquals(warehouse.addResourceToWarehouse(3,Resource.STONE), false);
         assertEquals(warehouse.getShelf(3).getResourceNumber(), 3);
-        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.MONEY);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
     }
 
     @Test
     public void addResourceToSpecialLevelTest(){
-
+        //primo piano
+        assertEquals(warehouse.addResourceToSpecialLevel(1,Resource.SHIELD), false);
+        warehouse.unlockLeaderLevel(Resource.SHIELD);
+        assertEquals(warehouse.addResourceToSpecialLevel(1,Resource.SHIELD), true);
+        assertEquals(warehouse.getLeaderLevelType(1), Resource.SHIELD);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 1);
+        assertEquals(warehouse.addResourceToSpecialLevel(1, Resource.SHIELD), true);
+        assertEquals(warehouse.addResourceToSpecialLevel(1, Resource.SHIELD), false);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 2);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceType(), Resource.SHIELD);
+        //secondo piano
+        assertEquals(warehouse.addResourceToSpecialLevel(2,Resource.SHIELD), false);
+        warehouse.unlockLeaderLevel(Resource.SHIELD);
+        assertEquals(warehouse.addResourceToSpecialLevel(2,Resource.SHIELD), true);
+        assertEquals(warehouse.getLeaderLevelType(2), Resource.SHIELD);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 1);
+        assertEquals(warehouse.addResourceToSpecialLevel(2, Resource.SHIELD), true);
+        assertEquals(warehouse.addResourceToSpecialLevel(2, Resource.SHIELD), false);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 2);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceType(), Resource.SHIELD);
     }
 
     @Test
@@ -66,45 +76,61 @@ public class WarehouseTest {
         assertEquals(warehouse.discardResourceFromWarehouse(1), false);
         warehouse.addResourceToWarehouse(1,Resource.MONEY);
         warehouse.discardResourceFromWarehouse(1);
-        assertEquals(warehouse.getLeaderShelf(1).getResourceType(), Resource.EMPTY);
-        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 0);
+        assertEquals(warehouse.getShelf(1).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(1).getResourceNumber(), 0);
 
         //test legalità secondo piano
         assertEquals(warehouse.discardResourceFromWarehouse(2), false);
         warehouse.addResourceToWarehouse(2,Resource.MONEY);
         warehouse.discardResourceFromWarehouse(2);
-        assertEquals(warehouse.getLeaderShelf(2).getResourceType(), Resource.EMPTY);
-        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 0);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(2).getResourceNumber(), 0);
         for(int i = 0; i < 2; i++){
-            warehouse.addResourceToWarehouse(2,Resource.MONEY);
+            warehouse.addResourceToWarehouse(2,Resource.SHIELD);
         }
+
         warehouse.discardResourceFromWarehouse(2);
-        assertEquals(warehouse.getLeaderShelf(2).getResourceType(), Resource.MONEY);
-        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 1);
+        assertEquals(warehouse.getShelf(2).getResourceNumber(), 1);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.SHIELD);
 
         //test legalità terzo piano
         assertEquals(warehouse.discardResourceFromWarehouse(3), false);
-        warehouse.addResourceToWarehouse(3,Resource.MONEY);
+        warehouse.addResourceToWarehouse(3,Resource.STONE);
         warehouse.discardResourceFromWarehouse(3);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceType(), Resource.EMPTY);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceNumber(), 0);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 0);
         for(int i = 0; i < 3; i++){
-            warehouse.addResourceToWarehouse(3,Resource.MONEY);
+            warehouse.addResourceToWarehouse(3,Resource.STONE);
         }
         warehouse.discardResourceFromWarehouse(3);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceType(), Resource.MONEY);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceNumber(), 2);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 2);
         warehouse.discardResourceFromWarehouse(3);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceType(), Resource.MONEY);
-        assertEquals(warehouse.getLeaderShelf(3).getResourceNumber(), 2);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 1);
     }
-
+    @Test
     public void discardResourceFromSpecialLevel(){
-
+        warehouse.unlockLeaderLevel(Resource.MONEY);
+        for(int i = 0; i < 2; i++) {
+            warehouse.addResourceToSpecialLevel(1, Resource.MONEY);
+        }
+        warehouse.discardResourceFromSpecialLevel(1);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 1);
+        warehouse.discardResourceFromSpecialLevel(1);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 0);
+        warehouse.unlockLeaderLevel(Resource.SHIELD);
+        for(int i = 0; i < 2; i++) {
+            warehouse.addResourceToSpecialLevel(2, Resource.SHIELD);
+        }
+        warehouse.discardResourceFromSpecialLevel(2);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 1);
+        warehouse.discardResourceFromSpecialLevel(2);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 0);
     }
 
     @Test
-    public void addResourceToStock(){
+    public void addResourceToStockTest(){
         assertEquals(warehouse.addResourceToStock(Resource.EMPTY), false);
         assertEquals(warehouse.addResourceToStock(Resource.FAITHPOINT), false);
         warehouse.addResourceToStock(Resource.MONEY);
@@ -125,18 +151,62 @@ public class WarehouseTest {
     }
 
     @Test
-    public void moveResourceToStockTest(){
+    public void removeResourceWarehouse(){
+        //test legalità primo piano
+        assertEquals(warehouse.removeResourceWarehouse(1), false);
+        warehouse.addResourceToWarehouse(1,Resource.MONEY);
+        warehouse.removeResourceWarehouse(1);
+        assertEquals(warehouse.getShelf(1).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(1).getResourceNumber(), 0);
 
+        //test legalità secondo piano
+        assertEquals(warehouse.removeResourceWarehouse(2), false);
+        warehouse.addResourceToWarehouse(2,Resource.MONEY);
+        warehouse.removeResourceWarehouse(2);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(2).getResourceNumber(), 0);
+        for(int i = 0; i < 2; i++){
+            warehouse.addResourceToWarehouse(2,Resource.SHIELD);
+        }
+
+        warehouse.removeResourceWarehouse(2);
+        assertEquals(warehouse.getShelf(2).getResourceNumber(), 1);
+        assertEquals(warehouse.getShelf(2).getResourceType(), Resource.SHIELD);
+
+        //test legalità terzo piano
+        assertEquals(warehouse.removeResourceWarehouse(3), false);
+        warehouse.addResourceToWarehouse(3,Resource.STONE);
+        warehouse.removeResourceWarehouse(3);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.EMPTY);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 0);
+        for(int i = 0; i < 3; i++){
+            warehouse.addResourceToWarehouse(3,Resource.STONE);
+        }
+        warehouse.removeResourceWarehouse(3);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 2);
+        warehouse.removeResourceWarehouse(3);
+        assertEquals(warehouse.getShelf(3).getResourceType(), Resource.STONE);
+        assertEquals(warehouse.getShelf(3).getResourceNumber(), 1);
     }
 
     @Test
-    public void discardResourceFromStockTest(){
-
+    public void removeSpecialResourceWarehouse(){
+        warehouse.unlockLeaderLevel(Resource.MONEY);
+        for(int i = 0; i < 2; i++) {
+            warehouse.addResourceToSpecialLevel(1, Resource.MONEY);
+        }
+        warehouse.removeSpecialResourceWarehouse(1);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 1);
+        warehouse.removeSpecialResourceWarehouse(1);
+        assertEquals(warehouse.getLeaderShelf(1).getResourceNumber(), 0);
+        warehouse.unlockLeaderLevel(Resource.SHIELD);
+        for(int i = 0; i < 2; i++) {
+            warehouse.addResourceToSpecialLevel(2, Resource.SHIELD);
+        }
+        warehouse.removeSpecialResourceWarehouse(2);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 1);
+        warehouse.removeSpecialResourceWarehouse(2);
+        assertEquals(warehouse.getLeaderShelf(2).getResourceNumber(), 0);
     }
-
-    @Test
-    public void removeResourceFromStockTest(){
-
-    }
-
 }

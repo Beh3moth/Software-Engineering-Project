f80322fd177849e6cad408ee16b3c7a834052c06
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-05-10 10:23:09 +0200

    Fixed CoordinatesMessage message type.

--- src/main/java/it/polimi/ngsw/network/message/ProductionPowerCoordinatesMessage.java
+++ src/main/java/it/polimi/ngsw/network/message/ProductionPowerCoordinatesMessage.java
@@ -8,7 +8,7 @@ public class ProductionPowerCoordinatesMessage extends CoordinatesMessage{
     private ProductionPower productionPower;
 
     public ProductionPowerCoordinatesMessage(String nickname, Boolean[] isWarehouse, Integer[] shelfLevel, Resource[] resourceType, ProductionPower productionPower) {
-        super(nickname, isWarehouse, shelfLevel, resourceType);
+        super(nickname, MessageType.PRODUCTION_POWER_COORDINATES_MESSAGE, isWarehouse, shelfLevel, resourceType);
         this.productionPower = productionPower;
     }
 

3fd234b6b80ecc2d216c251a7357d77d75a72fa6
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-05-11 10:58:30 +0200

    Added asciiArt to DevCardMarket and MarbleMarket

--- src/main/java/it/polimi/ngsw/view/cli/Cli.java
+++ src/main/java/it/polimi/ngsw/view/cli/Cli.java
@@ -632,12 +632,7 @@ public class Cli extends ViewObservable implements View {
     public void mainMove() {
         try {
 
-            for(int i=0; i<3; i++){
-                for (int j=0; j<4; j++){
-                    printDevCard(devCardMarket[i][j]);
-                }
-            }
-
+            printDevCardMarket();
             printMarket();
             printProductionPowerList(productionPowerList);
             out.println("Chose what you want to do: 1) Reorder warehouse 2) Take resources from the market 3) Buy a Development Card 4) Activate Production Powers");
@@ -707,19 +702,19 @@ public class Cli extends ViewObservable implements View {
     }
 
     public void printMarket() {
-        out.println("                   MARKET  ");
-        out.print("SINGLE MARBLE:                                     ");
-        out.println(this.singleMarble.getResource().toString());
+        out.println("MARKET");
+        out.print("SINGLE MARBLE: ");
+        out.println(getResourceArt(this.singleMarble.getResource()));
         for (int j = 0; j < 4; j++) {
-            out.print("  " + this.firstRow[j].getResource().toString() + "    ");
+            out.print("  " + getResourceArt(this.firstRow[j].getResource()) + "    ");
         }
         out.println("");
         for (int j = 0; j < 4; j++) {
-            out.print("  " + this.secondRow[j].getResource().toString() + "    ");
+            out.print("  " + getResourceArt(this.secondRow[j].getResource()) + "    ");
         }
         out.println("");
         for (int j = 0; j < 4; j++) {
-            out.print("  " + this.thirdRow[j].getResource().toString() + "    ");
+            out.print("  " + getResourceArt(this.thirdRow[j].getResource()) + "    ");
         }
         out.println("");
     }
@@ -1312,23 +1307,62 @@ public class Cli extends ViewObservable implements View {
         }
     }
 
+    private void printDevCardMarket(){
+
+        String[][] tilesArray = new String[31][80];
+
+
+        int pIterator = 0;
+        int kIterator = 0;
+
+        for(int i = 0; i < 31; i++){
+            for(int j = 0; j<80; j++){
+                tilesArray[i][j] = " ";
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 4; j++) {
+
+                for(int k = 0; k < MAX_VERT_TILES; k++) {
+
+                    for (int p = 0; p < MAX_HORIZON_TILES; p++) {
+                        tilesArray[k+(kIterator*10)][p+(pIterator*19)] = getPrintableDevCard(devCardMarket[i][j])[k][p];
+                    }
+
+                }
+
+                pIterator++;
+
+            }
+
+            kIterator++;
+            pIterator = 0;
+
+        }
+
+        for(int i = 0; i < 31; i++){
+            for(int j = 0; j<80; j++){
+                System.out.print(tilesArray[i][j]);
+            }
+            out.println();
+        }
+
+    }
+
     private static final int MAX_VERT_TILES = 9; //rows.
     private static final int MAX_HORIZON_TILES = 18; //cols.
 
-    private String tiles[][] = new String[MAX_VERT_TILES][MAX_HORIZON_TILES];
+    private String[][] tiles = new String[MAX_VERT_TILES][MAX_HORIZON_TILES];
 
-    public void printDevCard (DevCard devCard) {
+    public String[][] getPrintableDevCard (DevCard devCard) {
         fillEmpty();
         loadDevCardCost(devCard);
         loadDevCardLevel(devCard);
         loadDevCardProductionPower(devCard);
         loadPV(devCard);
-        for (int i=0; i<MAX_VERT_TILES; i++) {
-            for (int j = 0; j < MAX_HORIZON_TILES; j++) {
-                out.print(tiles[i][j]);
-            }
-            out.println();
-        }
+        return tiles;
     }
 
     private void loadPV(DevCard devCard){

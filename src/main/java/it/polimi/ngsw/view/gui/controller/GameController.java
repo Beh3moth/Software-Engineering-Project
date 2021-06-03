d0819c36b3aa80bd94b0591f4e661f02eed4be2e
samuele <samuele.cioffi@mail.polimi.it>
2021-06-03 18:37:45 +0200

    fixed bug and added chest in gui

--- src/main/java/it/polimi/ngsw/view/gui/controller/GameController.java
+++ src/main/java/it/polimi/ngsw/view/gui/controller/GameController.java
@@ -1,11 +1,13 @@
 package it.polimi.ngsw.view.gui.controller;
 
+import it.polimi.ngsw.model.Resource;
 import it.polimi.ngsw.observer.ViewObservable;
 import it.polimi.ngsw.view.LightModel;
 import it.polimi.ngsw.view.gui.SceneController;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
@@ -14,16 +16,28 @@ public class GameController extends ViewObservable implements GenericSceneContro
     private LightModel lightModel;
 
     @FXML
-    private Pane mainPane;
+    private Pane mainPain;
     @FXML
     private Button take_marble;
     @FXML
     private Button devCardMarket;
+    @FXML
+    private Label moneyNumber;
+    @FXML
+    private Label shieldNumber;
+    @FXML
+    private Label stoneNumber;
+    @FXML
+    private Label slaveNumber;
 
     @FXML
     public void initialize(){
-        mainPane.setVisible(true);
+        //mainPain.setVisible(true);
         take_marble.addEventHandler(MouseEvent.MOUSE_CLICKED, this::onTakeMarbleButtonClick);
+        moneyNumber.setText(lightModel.getChest().get(Resource.MONEY).toString());
+        shieldNumber.setText(lightModel.getChest().get(Resource.SHIELD).toString());
+        stoneNumber.setText(lightModel.getChest().get(Resource.STONE).toString());
+        slaveNumber.setText(lightModel.getChest().get(Resource.SLAVE).toString());
         devCardMarket.addEventHandler(MouseEvent.MOUSE_CLICKED, this::onDevCardMarket);
     }
 
@@ -42,5 +56,11 @@ public class GameController extends ViewObservable implements GenericSceneContro
         SceneController.changeScene(controller, "dev_card_market_scene.fxml");
     }
 
+    public void upDateValues(){
+        moneyNumber.setText(lightModel.getChest().get(Resource.MONEY).toString());
+        shieldNumber.setText(lightModel.getChest().get(Resource.SHIELD).toString());
+        stoneNumber.setText(lightModel.getChest().get(Resource.STONE).toString());
+        slaveNumber.setText(lightModel.getChest().get(Resource.SLAVE).toString());
+    }
 
 }

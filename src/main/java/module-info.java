c32c11fa0013dbf2886475237ada39d8b6e8c1a7
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-06-30 20:00:11 +0200

    Ultimate pom fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ open module it.polimi.ingsw {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires com.google.gson;
     requires java.desktop;
     requires java.logging;
+    requires com.google.gson;
 }
